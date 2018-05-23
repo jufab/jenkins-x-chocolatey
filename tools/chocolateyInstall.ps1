99657ea09f4b0b056c072179581402f091736faf
Julien FABRE <jufab@users.noreply.github.com>
2018-05-23 23:30:46 +0200

    initialisation

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'jenkins-x'
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+$url = 'https://github.com/jenkins-x/jx/releases/download/v1.2.77/jx-windows-amd64.zip'
+
+#Install-ChocolateyZipPackage -PackageName $packageName -Url $url -UnzipLocation "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName    = $packageName
+  url            = $url
+  unzipLocation  = $toolsPath
+}
+Install-ChocolateyZipPackage @packageArgs
+
+if (Test-Path "$toolsPath\jx-windows*.zip") {
+  $packageArgs2 = @{
+    packageName    = $packageName
+    url            = $url
+    unzipLocation  = $toolsPath
+  }
+  Install-ChocolateyZipPackage @packageArgs2
+
+  Remove-Item "$toolsPath\jx-windows*.zip"
+}
+
+#on renomme
+Rename-Item -Path "$toolsPath\jx-windows-amd64.exe" -NewName "jx.exe"
+
+#Il ne reste plus que le %PATH%
