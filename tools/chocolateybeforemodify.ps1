204971e57ba288325e5ceb176a137025c5ae869e
Julien FABRE <julien.fabre@pole-emploi.fr>
2018-06-28 19:10:54 +0200

    v1.3.42 + before

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,20 @@
+﻿write-host "Attempting to stop any running jx processes, to allow upgrade"
+try
+{
+  $running = get-process jx
+}
+catch
+{
+  write-host "No running jx processes to stop"
+  $running = @()
+}
+ 
+if ($running)
+{
+  write-host "Stopping jx processes"
+  foreach($p in $running)
+  {
+    stop-process $p
+    write-verbose "Stopped $($p.ProcessName) $($p.Id)"
+  }
+}
