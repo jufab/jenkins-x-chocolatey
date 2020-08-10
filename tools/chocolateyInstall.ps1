db00086c26db32cad0d0db2c8522307fe07f4094
Julien FABRE <julien.fabre.33@gmail.com>
2020-08-10 22:53:10 +0200

    fix: try to fix...

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,7 @@
 $packageName = 'jenkins-x'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$version = ([xml](Get-Content .\jenkins-x.nuspec)).package.metadata.version
+[xml]$nuspecContent = Get-Content .\jenkins-x.nuspec
+$version = $nuspecContent.package.metadata.version
 $fileZip = "jx-windows-amd64.zip"
 $fileChecksums = "jx-checksums.txt"
 $url = "https://github.com/jenkins-x/jx/releases/download/v$version/$fileZip"
