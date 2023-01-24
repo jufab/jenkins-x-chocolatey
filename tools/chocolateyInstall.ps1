44dcf7e21934ac249577d08491e534ec0c2a1900
Julien Fabre <julien.fabre.33@gmail.com>
2023-01-24 00:07:58 +0000

    fix: version

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,9 +2,7 @@ $checksum = "76790dac58330246bb52decbbcb028aa266f7b21a5f4d0dc38a0cb3dfd1f3ac2"
 $checksumtype = "sha256"
 $packageName = 'jenkins-x'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
-$nuspecPath = Resolve-Path -Path $toolsPath+'\..'
-[xml]$nuspecContent = Get-Content $nuspecPath\jenkins-x.nuspec
-$version = $nuspecContent.package.metadata.version
+$version = $env:ChocolateyPackageVersion
 $fileZip = "jx-windows-amd64.zip"
 $url = "https://github.com/jenkins-x/jx/releases/download/v$version/$fileZip"
 
