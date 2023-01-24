c73ef3c9985bb16afc5458acf3b3c15a4cc0600a
Julien Fabre <julien.fabre.33@gmail.com>
2023-01-24 00:00:39 +0000

    feat: new version checksum

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,17 +1,12 @@
+$checksum = "76790dac58330246bb52decbbcb028aa266f7b21a5f4d0dc38a0cb3dfd1f3ac2"
+$checksumtype = "sha256"
 $packageName = 'jenkins-x'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $nuspecPath = Resolve-Path -Path $toolsPath+'\..'
 [xml]$nuspecContent = Get-Content $nuspecPath\jenkins-x.nuspec
 $version = $nuspecContent.package.metadata.version
 $fileZip = "jx-windows-amd64.zip"
-$fileChecksums = "jx-checksums.txt"
 $url = "https://github.com/jenkins-x/jx/releases/download/v$version/$fileZip"
-$urlSHA = "https://github.com/jenkins-x/jx/releases/download/v$version/$fileChecksums"
-$fileSHA = "$toolsPath\$fileChecksums"
-Get-WebFile -Url $urlSHA -FileName $fileSHA
-$matchChecksum = Select-String -Path $fileSHA -Pattern "jx-windows-amd64\.zip$"
-$checksum = $matchChecksum.Line.Split(" ")[0]
-$checksumtype = "sha256" 
 
 if (Test-Path "$toolsPath\jx.exe") {
   Remove-Item "$toolsPath\jx.exe"
