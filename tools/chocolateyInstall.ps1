a7762c9702c9b66a1696bcd26e80798626ac7f64
Julien FABRE <julien.fabre.33@gmail.com>
2026-07-15 08:00:00 +0000

    3.17.42

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,4 +1,4 @@
-$checksum = "a20389ad71f5bb540195e208d4e9db0bf2cc9bba53f54a433654afa62409da3a"
+$checksum = "00716bb3163841f548b38a8686b67b51bee8f52f55464eca402b37a83e2d60e2"
 $checksumtype = "sha256"
 $packageName = 'jenkins-x'
 $toolsPath = Split-Path $MyInvocation.MyCommand.Definition
